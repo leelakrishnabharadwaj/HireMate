25306c8a76d650fa4a495394a33cf45a4b475c55
leelakrishnabharadwaj <bharat148vishnubhatla@gmail.com>
2021-09-10 23:57:51 +0530

    Initializing the applicant service

---
+++ applicant/src/main/java/com/hiremate/applicant/ApplicantApplication.java
@@ -0,0 +1,13 @@
+package com.hiremate.applicant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicantApplication.class, args);
+	}
+
+}
